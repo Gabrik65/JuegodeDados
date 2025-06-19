819ab9549a2214917cab81f45f71b13aa8121f74
nyest11 <nyest@aaa.com>
2025-06-19 09:20:57 -0400

    creado metodo ejecutar opcion

--- src/main/java/Controlador/JuegoDadosConsola.java
+++ src/main/java/Controlador/JuegoDadosConsola.java
@@ -6,22 +6,25 @@ import Vista.MenuOpciones;
 
 public class JuegoDadosConsola {
     private final String[] opciones = {"Lanzar Dados", "Mostrar Dados", "Sumar Dados","Salir"};
-    private MenuOpciones menuJuegoDados = new MenuOpciones("Juego de Dados",opciones);
-    private JuegoDados juego = new JuegoDados();
+    private final MenuOpciones menuJuegoDados = new MenuOpciones("Juego de Dados",opciones);
+    private final JuegoDados juego = new JuegoDados();
 
     public void menuJuego(){
         int opcion;
-        boolean salir=false;
+        boolean salir = false;
+
         do {
             menuJuegoDados.mostrarOpciones();
             opcion = menuJuegoDados.obtenerOpcionInt();
             if (menuJuegoDados.getCantidadOpciones() == opcion){
                 salir = menuJuegoDados.confirmarSalida();
             }
-            System.out.println("Eligio " + opcion + " por ende "+ menuJuegoDados.getNombreOpcion(opcion));
+            ejecutarOpcion(opcion);
         } while (opcion != menuJuegoDados.getCantidadOpciones() && salir);
     }
 
+    public void ejecutarOpcion(int opcion){
 
+    }
 
 }
