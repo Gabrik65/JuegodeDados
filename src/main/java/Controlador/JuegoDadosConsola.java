a5229f296811a5ad83aef6f67adc5223b3f7c6ae
nyest11 <nyest@aaa.com>
2025-06-19 09:05:39 -0400

    Metodo del ciclo

--- src/main/java/Controlador/JuegoDadosConsola.java
+++ src/main/java/Controlador/JuegoDadosConsola.java
@@ -1,10 +1,16 @@
 package Controlador;
 
+import Modelo.Dado;
 import Modelo.MenuOpciones;
 
 public class JuegoDadosConsola {
     private final String[] opciones = {"Lanzar Dados", "Mostrar Dados", "Sumar Dados","Salir"};
     private MenuOpciones menuJuegoDados = new MenuOpciones("Juego de Dados",opciones);
+    private Dado dado1 = new Dado();
+    private Dado dado2 = new Dado();
 
+    public void menuJuego(){
+
+    }
 
 }
