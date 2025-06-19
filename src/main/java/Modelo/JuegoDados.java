54c6d8bd13353b1469c63414ca87e02afdf8f033
Gabrik65 <g.valenzuela10@ufromail.cl>
2025-06-19 09:06:52 -0400

    Arreglando la Suma

--- src/main/java/Modelo/JuegoDados.java
+++ src/main/java/Modelo/JuegoDados.java
@@ -7,20 +7,28 @@ public class JuegoDados {
     private final Dado dado1;
     private final Dado dado2;
 
-    public JuegoDados() {
-        dado1 = new Dado();
-        dado2 = new Dado();
-        jugar();
 
+    public JuegoDados(Dado dado1, Dado dado2) {
+        this.dado1 = dado1;
+        this.dado2 = dado2;
+        int suma = jugar();
     }
 
-    public int[] jugar() {
+    public int jugar() {
         int valor1 = dado1.lanzar();
         int valor2 = dado2.lanzar();
-        return new int[]{valor1, valor2};
+        return valor1 + valor2;
     }
 
     public boolean esGanador(int suma) {
         return suma == 7;
+    }
+
+    public void resultadosJuego(int dado1, int dado2){
+        System.out.println("Resultados del Juego");
+        System.out.println("Primer dado: " + dado1);
+        System.out.println("Segundo dado: " + dado2);
+
+        System.out.println("Suma: " + dado1 + dado2);
     }
 }
