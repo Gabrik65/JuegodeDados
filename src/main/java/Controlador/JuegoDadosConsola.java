24e70b83a593daa01ac3b422756e87096f041f91
nyest11 <nyest@aaa.com>
2025-06-19 08:59:34 -0400

    Añadido clase MenuOpciones y JuegoDadosConsola

---
+++ src/main/java/Controlador/JuegoDadosConsola.java
@@ -0,0 +1,10 @@
+package Controlador;
+
+import Modelo.MenuOpciones;
+
+public class JuegoDadosConsola {
+    private final String[] opciones = {"Lanzar Dados", "Mostrar Dados", "Sumar Dados","Salir"};
+    private MenuOpciones menuJuegoDados = new MenuOpciones("Juego de Dados",opciones);
+
+
+}
